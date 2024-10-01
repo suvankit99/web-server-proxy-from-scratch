cec4474a34ba2b6148d327764bba26ce66e39fdc
suvankit99 <suvankit2001@gmail.com>
2024-10-01 16:11:12 +0530

    create cache element structure

---
+++ proxy_server_with_cache.c
@@ -0,0 +1,38 @@
+#include "proxy_parse.h"
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <pthread.h>
+
+#define MAX_CLIENTS 10 
+
+typedef struct cache_element cache_element ; 
+
+
+// LRU policy -> using timestamp of cache element 
+struct cache_element{
+    char * data ;
+    int len ; 
+    char * url ; 
+    time_t lru_time_track ; 
+    cache_element * next ; 
+};
+
+cache_element * find(char * url) ; 
+
+int add_cache_element(char * data , int size , char * url) ;
+
+void remove_cache_element() ;
+
+int port_number = 8080 ; 
+
+int proxy_socket_id ; 
+
+pthread_t tid[MAX_CLIENTS]  ;
+
+
+// for every thread with 
+sem_t semaphore ; 
+
+pthread_mutex_t lock ; 
+
